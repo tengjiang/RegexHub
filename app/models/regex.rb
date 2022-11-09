5cf79b44d237fd13deb245f76244d4297f19015e
ylwf <103905833+ylwf274@users.noreply.github.com>
2022-11-08 21:22:05 -0500

    Add files via upload

---
+++ app/models/regex.rb
@@ -0,0 +1,50 @@
+class Regex < ActiveRecord::Base
+    has_many :testcases
+    accepts_nested_attributes_for :testcases, :allow_destroy => true
+
+    validates :title, presence: { message: "Regex must have name!" }
+    validates :expression, presence: { message: "Regex must have expression!" }
+    # validates :expression, presence: true
+    # validates_associated :testcases
+
+
+    def self.parse_tag(tags)
+
+    end
+
+    def self.all_pass(exp, testcases)
+    end
+
+    def self.check_integrity(exp, str)
+        reg = Regexp.new exp
+        if str == '' or str.nil?
+            return "No input."
+        else
+            res = str =~ reg
+            res.nil?? "No match!": "Matches!"
+            # "First match at index #{res}."
+        end
+    end
+
+# functions for filter
+    def self.tags
+      pluck(:tag).uniq
+    end
+
+    def self.find_all_by_tags(tags, ordering)
+      self.where(tag: tags).order(ordering)
+    end
+
+    def self.all_tags
+      self.select(:tag).map(&:tag).uniq
+    end
+
+    def self.with_tags(tag)
+      if tag.nil?
+        return self.where(tag: self.all_tags)
+      else
+        return self.where(tag: tag)
+      end
+    end
+
+end
