8e1dc517a49eb3341b1711b14db0b2069eadf769
tengjiangpku <jiangteng@pku.edu>
2022-11-14 17:02:13 -0500

    Testcases cannot repeat

--- app/models/testcase.rb
+++ app/models/testcase.rb
@@ -2,5 +2,4 @@ class Testcase < ActiveRecord::Base
   belongs_to :regex
   validates :content, presence: { message: "All regexes must have title!" }
   validates :match, presence: { message: "All regexes must specify expected match result!" }
-
 end
