236a4882b3685b6ba59379db237e3bc754825f8c
tengjiangpku <jiangteng@pku.edu>
2022-11-15 17:52:27 -0500

    Finish Rspec

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,4 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
+    context '#create' do
+        it 'successful login' do
+            allow(User).to receive(:create)
+            User.create!({:username => 'test',:password =>'test'})
+            get :create, {:username=>"test", :password=>"test"}
+            expect(response).to redirect_to regexes_path
+        end
+        it 'cannot login with wrong password' do
+            allow(User).to receive(:create)
+            User.create!({:username => 'test',:password =>'test'})
+            get :create, {:username=>"test", :password=>"test1"}
+            expect(response).to redirect_to login_path
+        end
+        it 'cannot login with username that does not exist' do
+            allow(User).to receive(:create)
+            User.create!({:username => 'test',:password =>'test'})
+            get :create, {:username=>"test1", :password=>"test1"}
+            expect(response).to redirect_to login_path
+        end
+    end
+    context '#destroy' do
+        it 'log out successfull' do
+            get :destroy
+            expect(response).to redirect_to regexes_path
+        end
+    end
+
+    
 end
