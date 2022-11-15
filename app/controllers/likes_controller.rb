6ca651476174f64258411dba1868027ab2f4a8a4
ylwf274 <ylxiao2022@outlook.com>
2022-11-15 13:59:01 -0500

    add like function

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,17 +1,24 @@
 class LikesController < ApplicationController
-    def create
-        # @like = current_user.likes.new(like_params)
-        @like = Like.new(like_params)
-        if !@like.save
-            flash[:notice] = @like.errors.full_messages.to_sentence
-        end
+    # before_action :authenticate_user!
 
-        redirect_to @like.regex
+    def create
+        @like = (current_user ? current_user.likes.new(like_params):nil)
+        # @like = Like.new(like_params)
+        if @like
+            if !@like.save
+                flash[:notice] = @like.errors.full_messages.to_sentence
+            end
+            redirect_to @like.regex
+        else
+            flash[:notice] = "only users can do this!"
+            redirect_to login_path
+        end
+        
     end
     
     def destroy
-        # @like = current_user.likes.find(params[:id])
-        @like = Like.find(params[:id])
+        @like = current_user.likes.find(params[:id])
+        # @like = Like.find(params[:id])
         regex = @like.regex
         @like.destroy
         redirect_to regex
