bb6286bceda4d61dd92b774126ec865b132fe4f6
tengjiangpku <jiangteng@pku.edu>
2022-11-15 16:30:59 -0500

    user controller spec

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,4 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+    context '#create' do
+        it 'successful create a user' do
+            get :create, {:user=>{:username=>"test", :password=>"test",:password_confirmation=>"test"}}
+            expect(response).to redirect_to regexes_path
+        end
+        it 'cannot create a user when the passowrd is wrong' do
+            get :create, {:user=>{:username=>"test", :password=>"test",:password_confirmation=>"test1"}}
+            expect(response).to render_template("new")
+        end
+    end
+
 end
